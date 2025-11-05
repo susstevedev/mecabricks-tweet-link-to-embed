e19a38df4cdecb14c533b5fae277fe22528b03de
Evan <168851101+susstevedev@users.noreply.github.com>
2025-11-05 17:19:26 -0600

    Update index.user.js

--- index.user.js
+++ index.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Mecabricks - Tweet link to embed
 // @namespace    http://tampermonkey.net/
-// @version      2025-11-04
+// @version      2025-11-05
 // @description  Replaces twitter.com links with embeds on Mecabricks
 // @author       susstevedev
-// @match        https://*.mecabricks.com/*/forum/topic/*
+// @match        https://*.mecabricks.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @require      https://platform.twitter.com/widgets.js
@@ -12,16 +12,23 @@
 
 (function() {
     'use strict';
-  
-    const posts = document.querySelectorAll('.post .message-wrapper > .text');
-    console.log(posts);
 
-    posts.forEach(p => {
-        console.log(p);
+    const WINDOW_URL = window.location.href
+    if(WINDOW_URL.includes('forum')) {
+        const posts = document.querySelectorAll('.post .message-wrapper > .text');
+        console.log(posts);
 
-        p.querySelectorAll('a[href*="twitter.com"]').forEach(a => {
-            const url = a.href;
-            a.outerHTML = `<blockquote class="twitter-tweet"><a href="${url}"></a></blockquote>`;
+        posts.forEach(p => {
+            console.log(p);
+
+            p.querySelectorAll('a[href*="twitter.com"]').forEach(a => {
+                const url = a.href;
+                a.outerHTML = `<blockquote class="twitter-tweet"><a href="${url}"></a></blockquote>`;
+            });
         });
-    });
+    } else {
+        const twtwrapper = document.querySelector('#superglobal #twitter-wrapper #twitter-timeline');
+        document.querySelector('#twitter-title').innerText = "@Mecabricks on X";
+        twtwrapper.innerHTML = `<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/Mecabricks?ref_src=twsrc%5Etfw">Posts by Mecabricks</a>`;
+    }
 })();
